c6d9daed96062ae80ff0019e2f526e5770f7b379
jackbloo <a.iqbal1404@gmail.com>
2019-05-29 12:00:53 +0700

    revisi lagi

--- MengelompokkanHewan.js
+++ MengelompokkanHewan.js
@@ -1,57 +1,33 @@
-
 function groupAnimals(animals) {
-    // you can only write your code here!
-animals.sort()
-var arr2D = [];
-var firstWordAnimal = animals[0][0]
-var arr1D = [];
-for(var i = 0; i < animals.length;i++){
-  if(firstWordAnimal === animals[i][0]){
-        arr1D.push(animals[i])
-  } else {
-    arr2D.push(arr1D)
-    arr1D = [];
-    firstWordAnimal = animals[i][0]
-    arr1D.push(animals[i])
-  } if (i === animals.length-1){
-    arr2D.push(arr1D)
-  }
-}
-      return arr2D
-  }
-
-
-  function groupAnimals(animals) {
-    // you can only write your code here!
+  // you can only write your code here!
 animals.sort(function(a,b){return a>b})
 var arr2D = [];
 for(var i = 0; i < animals.length;i++){
-  var firstWordAnimal = animals[i][0]
-  var arr1D = [];
-  for(var j = 0;j < animals.length; j++){
-  if(firstWordAnimal === animals[j][0]){
-        arr1D.push(animals[j])
-        j= j-1
 
+var firstWordAnimal = animals[i][0]
+var arr1D = [];
+  for(var j = i; j < animals.length; j++){
+    if(firstWordAnimal === animals[j][0]){
+      
+      arr1D.push(animals[j])
+      animals.splice(j, 1)
+      j--
+      i=-1
   }
-  }
-
-      arr2D.push(arr1D)
 }
-    arr2D.splice(0,1)
-    arr2D.splice(2,1)
-    arr2D.splice(3,1)
-    return arr2D
+
+    arr2D.push(arr1D)
+}
+  return arr2D
+
+}
 
 
-  }
 
 
-
-  
-  // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
 
